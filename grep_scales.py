6c5cfe23c3b76b1159903e75dc6dd7bedd70dc5a
Ian Miell <ian.miell@gmail.com>
2016-03-28 10:56:32 +0100

    grep scales

--- grep_scales.py
+++ grep_scales.py
@@ -1,79 +1,9 @@
-"""ShutIt module. See http://shutit.tk
-"""
-
 from shutit_module import ShutItModule
-
 
 class grep_scales(ShutItModule):
 
-
 	def build(self, shutit):
-		# Some useful API calls for reference. See shutit's docs for more info and options:
-		#
-		# ISSUING BASH COMMANDS
-		# shutit.send(send,expect=<default>) - Send a command, wait for expect (string or compiled regexp)
-		#                                      to be seen before continuing. By default this is managed
-		#                                      by ShutIt with shell prompts.
-		# shutit.multisend(send,send_dict)   - Send a command, dict contains {expect1:response1,expect2:response2,...}
-		# shutit.send_and_get_output(send)   - Returns the output of the sent command
-		# shutit.send_and_match_output(send, matches) 
-		#                                    - Returns True if any lines in output match any of 
-		#                                      the regexp strings in the matches list
-		# shutit.send_until(send,regexps)    - Send command over and over until one of the regexps seen in the output.
-		# shutit.run_script(script)          - Run the passed-in string as a script
-		# shutit.install(package)            - Install a package
-		# shutit.remove(package)             - Remove a package
-		# shutit.login(user='root', command='su -')
-		#                                    - Log user in with given command, and set up prompt and expects.
-		#                                      Use this if your env (or more specifically, prompt) changes at all,
-		#                                      eg reboot, bash, ssh
-		# shutit.logout(command='exit')      - Clean up from a login.
-		# 
-		# COMMAND HELPER FUNCTIONS
-		# shutit.add_to_bashrc(line)         - Add a line to bashrc
-		# shutit.get_url(fname, locations)   - Get a file via url from locations specified in a list
-		# shutit.get_ip_address()            - Returns the ip address of the target
-		# shutit.command_available(command)  - Returns true if the command is available to run
-		#
-		# LOGGING AND DEBUG
-		# shutit.log(msg,add_final_message=False) -
-		#                                      Send a message to the log. add_final_message adds message to
-		#                                      output at end of build
-		# shutit.pause_point(msg='')         - Give control of the terminal to the user
-		# shutit.step_through(msg='')        - Give control to the user and allow them to step through commands
-		#
-		# SENDING FILES/TEXT
-		# shutit.send_file(path, contents)   - Send file to path on target with given contents as a string
-		# shutit.send_host_file(path, hostfilepath)
-		#                                    - Send file from host machine to path on the target
-		# shutit.send_host_dir(path, hostfilepath)
-		#                                    - Send directory and contents to path on the target
-		# shutit.insert_text(text, fname, pattern)
-		#                                    - Insert text into file fname after the first occurrence of 
-		#                                      regexp pattern.
-		# shutit.delete_text(text, fname, pattern)
-		#                                    - Delete text from file fname after the first occurrence of
-		#                                      regexp pattern.
-		# shutit.replace_text(text, fname, pattern)
-		#                                    - Replace text from file fname after the first occurrence of
-		#                                      regexp pattern.
-		# ENVIRONMENT QUERYING
-		# shutit.host_file_exists(filename, directory=False)
-		#                                    - Returns True if file exists on host
-		# shutit.file_exists(filename, directory=False)
-		#                                    - Returns True if file exists on target
-		# shutit.user_exists(user)           - Returns True if the user exists on the target
-		# shutit.package_installed(package)  - Returns True if the package exists on the target
-		# shutit.set_password(password, user='')
-		#                                    - Set password for a given user on target
-		#
-		# USER INTERACTION
-		# shutit.get_input(msg,default,valid[],boolean?,ispass?)
-		#                                    - Get input from user and return output
-		# shutit.fail(msg)                   - Fail the program and exit with status 1
-		# 
-		afile = r'''
-THIS LINE IS THE 1ST UPPER CASE LINE IN THIS FILE.
+		afile = r'''THIS LINE IS THE 1ST UPPER CASE LINE IN THIS FILE.
 this line is the 1st lower case line in this file.
 This Line Has All Its First Character Of The Word With Upper Case.
 
@@ -83,41 +13,29 @@ And this is the last line.
 		shutit.send_file('afile',afile)
 		shutit.send('alias grep=grep')
 		shutit.golf('''I have created a file called 'afile' that looks like this: 
-=================== BEGIN afile
+BEGINS
 ''' + afile + '''
-=================== END afile
-
-For your first task, grep out the last line.
+ENDS
+For your first task, grep out the last line 'And this is the last line.'.
 ''','And this is the last line.')
+		shutit.golf('Return a count of the number of lines with "UPPER" in it (case sensitive)','1')
+		shutit.golf('Return a count of the number of lines with "UPPER" in it (case insensitive)','2')
+		shutit.golf('Return lines that have the word "in" in it (case insensitive)','264200b0557e7c2e75cffc57778311f4',expect_type='md5sum')
+		shutit.golf('''Return lines that DON'T have the word 'case' (case insensitive) in it.''','ca75d0d8558569109e342ac5e09c4d01',expect_type='md5sum')
+		shutit.golf('''Return line with "UPPER" in it, along with the line number.''','cc9246de53156c4259be5bf05dacadf6',expect_type='md5sum')
+		shutit.golf('Print the line after the empty line.','63b6f5fd46648742a6f7aacff644dd92',expect_type='md5sum',print_md5=True)
+		shutit.golf('Print the two lines before the empty line.','444cc6679be200fc6579678b6afe19e9',expect_type='md5sum',print_md5=True)
+		shutit.golf('Print the lines before, the empty line, and the line after.','7ba4233c4599e0aefd11e93a66c4bf17',expect_type='md5sum')
+		#-o, --only-matching Print only the matched (non-empty) parts of a matching line, with each such part on a separate output line.
+		#-l, --files-with-matches Suppress normal output; instead print the name of each input file from which output would normally have been printed.  The scanning will stop on the first match.
+		#-r
 		return True
-
-	def get_config(self, shutit):
-		# CONFIGURATION
-		# shutit.get_config(module_id,option,default=None,boolean=False)
-		#                                    - Get configuration value, boolean indicates whether the item is 
-		#                                      a boolean type, eg get the config with:
-		# shutit.get_config(self.module_id, 'myconfig', default='a value')
-		#                                      and reference in your code with:
-		# shutit.cfg[self.module_id]['myconfig']
-		return True
-
-	def test(self, shutit):
-		# For test cycle part of the ShutIt build.
-		return True
-
-	def finalize(self, shutit):
-		# Any cleanup required at the end.
-		return True
-	
-	def is_installed(self, shutit):
-		return False
-
 
 def module():
 	return grep_scales(
 		'tk.shutit.grep_scales.grep_scales', 1845506479.0001,
-		description='',
-		maintainer='',
+		description='Practice your grep scales!',
+		maintainer='ian.miell@gmail.com',
 		delivery_methods=['docker'],
 		depends=['shutit.tk.setup']
 	)
