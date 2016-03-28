20703ed280963d61b01e29a5cbda53638b1407f1
Ian Miell <ian.miell@gmail.com>
2016-03-28 11:17:35 +0100

    no printing md5sum

--- grep_scales.py
+++ grep_scales.py
@@ -23,8 +23,8 @@ For your first task, grep out the last line 'And this is the last line.'.
 		shutit.golf('Return lines that have the word "in" in it (case insensitive)','264200b0557e7c2e75cffc57778311f4',expect_type='md5sum')
 		shutit.golf('''Return lines that DON'T have the word 'case' (case insensitive) in it.''','ca75d0d8558569109e342ac5e09c4d01',expect_type='md5sum')
 		shutit.golf('''Return line with "UPPER" in it, along with the line number.''','cc9246de53156c4259be5bf05dacadf6',expect_type='md5sum')
-		shutit.golf('Print the line after the empty line.','63b6f5fd46648742a6f7aacff644dd92',expect_type='md5sum',print_md5=True)
-		shutit.golf('Print the two lines before the empty line.','444cc6679be200fc6579678b6afe19e9',expect_type='md5sum',print_md5=True)
+		shutit.golf('Print the line after the empty line.','63b6f5fd46648742a6f7aacff644dd92',expect_type='md5sum')
+		shutit.golf('Print the two lines before the empty line.','444cc6679be200fc6579678b6afe19e9',expect_type='md5sum')
 		shutit.golf('Print the lines before, the empty line, and the line after.','7ba4233c4599e0aefd11e93a66c4bf17',expect_type='md5sum')
 		#-o, --only-matching Print only the matched (non-empty) parts of a matching line, with each such part on a separate output line.
 		#-l, --files-with-matches Suppress normal output; instead print the name of each input file from which output would normally have been printed.  The scanning will stop on the first match.
