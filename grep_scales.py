173b26ccb86fe90fa2f4578ea060f2be7c2203af
Ian Miell <ian.miell@gmail.com>
2016-04-05 10:58:28 +0100

    hints

--- grep_scales.py
+++ grep_scales.py
@@ -18,14 +18,14 @@ BEGINS
 ENDS
 For your first task, grep out the last line 'And this is the last line.'.
 ''','And this is the last line.',hints=['last','grep last afile'])
-		shutit.golf('Return a count of the number of lines with "UPPER" in it (case sensitive)','1',hints=['-c'])
-		shutit.golf('Return a count of the number of lines with "UPPER" in it (case insensitive)','2',hints=['-c','-i'])
-		shutit.golf('Return lines that have the word "in" in it (case insensitive)','264200b0557e7c2e75cffc57778311f4',expect_type='md5sum',hints=['-w','-i'])
-		shutit.golf('''Return lines that DON'T have the word 'case' (case insensitive) in it.''','ca75d0d8558569109e342ac5e09c4d01',expect_type='md5sum',hints=['-v','-i'])
-		shutit.golf('''Return line with "UPPER" in it, along with the line number.''','cc9246de53156c4259be5bf05dacadf6',expect_type='md5sum',hints=['-n'])
-		shutit.golf('Print the line after the empty line.','63b6f5fd46648742a6f7aacff644dd92',expect_type='md5sum',hints=['-A','-A1'])
-		shutit.golf('Print the two lines before the empty line.','444cc6679be200fc6579678b6afe19e9',expect_type='md5sum',hints=['-B','-B2'])
-		shutit.golf('Print the line before the empty line, the empty line, and the line after.','7ba4233c4599e0aefd11e93a66c4bf17',expect_type='md5sum',hints=['-C','-C1'],congratulations='Well done, all done!')
+		shutit.golf('Return a count of the number of lines with "UPPER" in it (case sensitive)','1',hints=['-c','ask again to get answer','grep -c UPPER afile'])
+		shutit.golf('Return a count of the number of lines with "UPPER" in it (case insensitive)','2',hints=['-c','-i','ask again to get answer','grep -c -i UPPER afile'])
+		shutit.golf('Return lines that have the word "in" in it (case insensitive)','264200b0557e7c2e75cffc57778311f4',expect_type='md5sum',hints=['-w','-i','ask again to get answer','grep -w -i in afile'])
+		shutit.golf('''Return lines that DON'T have the word 'case' (case insensitive) in it.''','ca75d0d8558569109e342ac5e09c4d01',expect_type='md5sum',hints=['-v','-i','ask again to get answer','grep -v case afile'])
+		shutit.golf('''Return line with "UPPER" in it, along with the line number.''','cc9246de53156c4259be5bf05dacadf6',expect_type='md5sum',hints=['-n','ask again to get answer','grep -n UPPER afile'])
+		shutit.golf('Print the line after the empty line.','63b6f5fd46648742a6f7aacff644dd92',expect_type='md5sum',hints=['-A','-A1','ask again to get answer','grep -A1 ^$ afile'])
+		shutit.golf('Print the two lines before the empty line.','444cc6679be200fc6579678b6afe19e9',expect_type='md5sum',hints=['-B','-B2','ask again to get answer','grep -B2 ^$ afile'])
+		shutit.golf('Print the line before the empty line, the empty line, and the line after.','7ba4233c4599e0aefd11e93a66c4bf17',expect_type='md5sum',hints=['-C','-C1','ask again to get answer','grep -C1 ^$ afile'],congratulations='Well done, all done!')
 		#-o, --only-matching Print only the matched (non-empty) parts of a matching line, with each such part on a separate output line.
 		#-l, --files-with-matches Suppress normal output; instead print the name of each input file from which output would normally have been printed.  The scanning will stop on the first match.
 		#-r
